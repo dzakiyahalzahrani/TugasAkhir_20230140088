2bbc5c956846267b828769cd63911709be170ac4
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2026-01-16 14:08:13 +0700

    membuat File NavRoutes

---
+++ app/src/main/java/com/example/tugasakhir_20230140088/navigation/NavRoutes.kt
@@ -0,0 +1,11 @@
+package com.example.tugasakhir_20230140088.navigation
+
+object NavRoutes {
+    const val AUTH = "auth"
+    const val REGISTER = "register"
+    const val FOCUS = "focus"
+    const val STATISTICS = "statistics"
+    const val SETTINGS = "settings"
+    const val TEMPLATES = "templates"
+
+}
